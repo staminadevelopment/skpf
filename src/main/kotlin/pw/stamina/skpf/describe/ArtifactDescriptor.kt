425e2d29b6309b318c01810eea25ae85b2cf8a1a
feature <15078089+feature@users.noreply.github.com>
2018-07-10 10:00:48 +0200

    Document descriptors

--- src/main/kotlin/pw/stamina/skpf/describe/ArtifactDescriptor.kt
+++ src/main/kotlin/pw/stamina/skpf/describe/ArtifactDescriptor.kt
@@ -2,9 +2,27 @@ package pw.stamina.skpf.describe
 
 import com.github.zafarkhaja.semver.Version
 
+/**
+ * Provides the identifiable information about the plugin.
+ */
 class ArtifactDescriptor(
+        /**
+         * The identifier of the group the plugin belongs to.
+         */
         val groupId: String,
+
+        /**
+         * The plugin artifact identifier.
+         */
         val artifactId: String,
+
+        /**
+         * The current version of the plugin.
+         */
         val version: Version,
+
+        /**
+         * An optional friendly name for the plugin.
+         */
         val name: String?
 )
--- src/main/kotlin/pw/stamina/skpf/describe/InstantiationDescriptor.kt
+++ src/main/kotlin/pw/stamina/skpf/describe/InstantiationDescriptor.kt
@@ -1,5 +1,20 @@
 package pw.stamina.skpf.describe
 
+/**
+ * Provides the information necessary for instantiation of
+ * the plugin.
+ */
 class InstantiationDescriptor(
-        val mainClass: String
+        /**
+         * The fully qualified name of the class to be used
+         * when instantiating the plugin.
+         */
+        val mainClass: String,
+
+        /**
+         * Specifies what strategy to use when attempting
+         * to instantiate the plugin. If none is specified
+         * a default strategy should be used.
+         */
+        val strategy: String?
 )
