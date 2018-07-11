d3a7799619be2babde777beb3cf8965efec24011
feature <15078089+feature@users.noreply.github.com>
2018-07-11 02:06:35 +0200

    Add matches function to dependency
    Document dependency class

--- src/main/kotlin/pw/stamina/skpf/depend/Dependency.kt
+++ src/main/kotlin/pw/stamina/skpf/depend/Dependency.kt
@@ -1,14 +1,46 @@
 package pw.stamina.skpf.depend
 
+import com.github.zafarkhaja.semver.Version
 import com.github.zafarkhaja.semver.expr.Expression
+import pw.stamina.skpf.describe.ArtifactDescriptor
 
 class Dependency(
+        /**
+         * The identifier of the group the dependency
+         * belongs to.
+         */
+        val groupId: String,
 
-    val groupId: String,
+        /**
+         * The artifact identifier of the dependency.
+         */
+        val artifactId: String,
 
-    val artifactId: String,
+        /**
+         * An optional version matcher, for targeting
+         * only specific versions of the dependency.
+         */
+        val versionMatcher: Expression?,
 
-    val versionMatcher: Expression?,
+        /**
+         * Indicates if the dependency is in not required
+         * for the dependant to function.
+         */
+        val optional: Boolean) {
 
-    val optional: Boolean
-)
+    /**
+     * Checks if the [artifact] descriptor matches the
+     * target of this dependency.
+     */
+    fun matches(artifact: ArtifactDescriptor) =
+            groupId != artifact.groupId
+         && artifactId != artifact.artifactId
+         && matchVersion(artifact.version)
+
+    /**
+     * Checks if the [version] matches the version matcher
+     * of this dependency. This function always returns
+     * `true` if no version matcher has been specified.
+     */
+    private fun matchVersion(version: Version) = versionMatcher?.interpret(version) ?: true
+}
